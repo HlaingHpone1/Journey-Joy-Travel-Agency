296b07e48223bf19d4217f0ee1fc6ff9d86628de
JiJi9526 <maynine952006@gmail.com>
2024-02-11 10:37:42 +0630

    push

--- src/components/form/Login.jsx
+++ src/components/form/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword]= useState("");
+  const emailHandler = (e)=>{
+    setEmail(e.target.value)
+}
+
+const passwordHandler = (e)=>{
+  setPassword(e.target.value)
+}
+
+const addHandler = ()=>{
+  console.log("Added value "+ email , password)
+  if(email, password !== ""){
+      setEmail("");
+      setPassword("");
+  }
+}
+
   return (
-    <div>Login
-        
+    <div>
+      <div className='signIn'>
+        <input type='email' placeholder='Email' value={email}
+        onChange={emailHandler}/><br/>
+        <input type='password' placeholder='Password' value={password} onChange={passwordHandler}/><br/>
+
+        <button type='button' onClick={addHandler}  to="/">Submit</button>
+      </div>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Image } from '../components/img/Image'
+import { SlSocialInstagram } from "react-icons/sl";
+import { TiSocialTwitter } from "react-icons/ti";
+import { TiSocialFacebook } from "react-icons/ti";
+
+
 
 const Login = () => {
+	const submitHandler = (e)=>{}
+        console.log("done")
 	return (
-		<div>
-		
+		<div className='login bg-primary'>
+			<div className='inner-wrap max-w-1240px my-0 mx-auto'>
+				<div className='login-box flex'>
+					<div className='login-box_img'>
+						
+					</div>
+
+					<div className='login-box_text my-0 mx-auto'>
+					<h2>Log In</h2>
+						<form onSubmit={submitHandler}>
+							<div className='socialIcon flex'>
+								<div className='facebook'>
+									<TiSocialFacebook />
+								</div>
+								<div className='twitter'>
+									<TiSocialTwitter />
+								</div>
+								<div className='twitter'>
+									<SlSocialInstagram />
+								</div>
+							</div>
+							<Login/>
+							<p>Or</p>
+							<p>Forgot your password?</p>
+							<p>Don't have an account? <a href="">Register</a></p> 
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
