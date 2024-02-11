d25737f4a534e5805d3f47c7b1c509d4c0def8e3
JiJi9526 <maynine952006@gmail.com>
2024-02-11 13:52:52 +0630

    log in

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
+import Banner from '../components/banner/Banner';
+import { Banner as ImgBanner, Image } from "../components/img/Image"
+
 
 const Review = () => {
     return (
-        <div className='review bg-primary'>
-            <div className='inner-wrap max-w-1240px my-0 mx-auto'>
-                <h2>Testimonials</h2>
-                <div className='review-slider flex'>
-                    <IoArrowBackCircleOutline />
-                    <IoArrowForwardCircleOutline />
+        <>
+            <Banner
+                image={ImgBanner.Banner3}
+                header="Package"
+                text="Home / Pages / Package"
+            />
+            <div className='review bg-primary'>
+                
+                <div className='inner-wrap max-w-1240px my-0 mx-auto'>
+                    <h2>Testimonials</h2>
+                    <div className='review-slider flex'>
+                        <IoArrowBackCircleOutline />
+                        <IoArrowForwardCircleOutline />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
