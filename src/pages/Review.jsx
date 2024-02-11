602cc091d21bd50f6fb35a832102b291c7332269
JiJi9526 <maynine952006@gmail.com>
2024-02-11 15:59:13 +0630

    review

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import Banner from '../components/banner/Banner';
-import { Banner as ImgBanner, Image } from "../components/img/Image"
-
+import { Banner as ImgBanner} from "../components/img/Image"
+import {User} from "../components/img/Image";
+import Testimonial from '../components/testimonial/Testimonial';
 
 const Review = () => {
     return (
@@ -14,12 +15,31 @@ const Review = () => {
                 text="Home / Pages / Package"
             />
             <div className='review bg-primary'>
-                
-                <div className='inner-wrap max-w-1240px my-0 mx-auto'>
-                    <h2>Testimonials</h2>
-                    <div className='review-slider flex'>
-                        <IoArrowBackCircleOutline />
-                        <IoArrowForwardCircleOutline />
+                <div className='inner-wrap max-w-1240px my-0 mx-auto text-center pt-36 pb-12'>
+                    <h2 className='text-6xl font-Roboto-Slab text-white'>Testimonials</h2>
+                    <div className="review-box flex space-x-12 mt-9 mb-6">
+
+                    <Testimonial
+                        userImage={User.User1}
+                        name="John"
+                        review="The travel experience exceeded expectations—seamless booking, thrilling adventures, and a perfect mix of exploration. A definite go-to for adventure seekers!"
+                        />
+
+                    <Testimonial
+                        userImage={User.User2}
+                        name="Jennifer"
+                        review="Just had the perfect blend of adventure and relaxation on my getaway booked here. Varied experiences catered to all preferences, from thrilling activities to serene beach moments. Kudos for the diverse and balanced travel packages!"
+                        />
+
+                    <Testimonial
+                        userImage={User.User3}
+                        name="Sam"
+                        review="Guided tours provided deep cultural dives, balancing popular attractions with off-the-beaten-path adventures. Highly recommended for authentic explorations!"
+                        />
+                    </div>
+                    <div className='review-slider flex justify-center'>
+                        <IoArrowBackCircleOutline className='size-16 text-tertiary'/>
+                        <IoArrowForwardCircleOutline className='size-16 text-tertiary'/>
                     </div>
                 </div>
             </div>
