80c3ed07b4fc3e77c387a39fa8c70bbbfaa30120
Sett-Paing <settpaing292@example.com>
2024-02-11 14:29:28 +0630

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import { Image } from '../components/img/Image';
 import { LiaPhoneVolumeSolid } from "react-icons/lia";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { MdMarkEmailUnread } from "react-icons/md";
-
+import { Banner } from '../components/banner/Banner';
 
 const Contact = () => {
     const handleSubmit = (formData) => {
@@ -12,33 +12,37 @@ const Contact = () => {
     };
 
     return (
-        <div className="bg-primary">
-            <div className="inner-wrap max-w-1240px my-0 mx-auto">
-                <div className="text-center text-white font-Roboto-Slab">
-                    <h1 className="text-3xl font-bold mb-4">Get in touch with us</h1>
-                    <div className="grid grid-cols-3 gap-4 mb-8">
-                        <div>
-                        <FaMapLocationDot className='block m-0 mx-auto size-9'  />
-                            <h2 className="text-xl font-bold mb-2">Location</h2>
-                            <p>No.225, Yaw Gi kyung Street, Hlaing Township, Yangon</p>
+        <div className="">
+            <Banner />
+            <div className="bg-primary">
+                <div className="inner-wrap max-w-1240px my-0 mx-auto">
+                    <div className="text-center text-white font-Roboto-Slab">
+                        <h1 className="text-3xl font-bold mb-4">Get in touch with us</h1>
+                        <div className="grid grid-cols-3 gap-4 mb-8">
+                            <div>
+                                <FaMapLocationDot className='block m-0 mx-auto size-9' />
+                                <h2 className="text-xl font-bold mb-2">Location</h2>
+                                <p>No.225, Yaw Gi kyung Street, Hlaing Township, Yangon</p>
+                            </div>
+                            <div>
+                                <LiaPhoneVolumeSolid className='block m-0 mx-auto size-9' />
+                                <h2 className="text-xl font-bold mb-2">Phone</h2>
+                                <p>01264445</p>
+                                <p>+95 9779779779</p>
+                            </div>
+                            <div>
+                                <MdMarkEmailUnread className='block m-0 mx-auto size-9' />
+                                <h2 className="text-xl font-bold mb-2">Email</h2>
+                                <p>info@journeyjoy.com</p>
+                            </div>
                         </div>
-                        <div>
-                        <LiaPhoneVolumeSolid className='block m-0 mx-auto size-9' />
-                            <h2 className="text-xl font-bold mb-2">Phone</h2>
-                            <p>01264445</p>
-                            <p>+95 9779779779</p>
-                        </div>
-                        <div>
-                          <MdMarkEmailUnread className='block m-0 mx-auto size-9' />
-                            <h2 className="text-xl font-bold mb-2">Email</h2>
-                            <p>info@journeyjoy.com</p>
-                        </div>
+                        <ContactForm onSubmit={handleSubmit} />
+
                     </div>
-                    <ContactForm onSubmit={handleSubmit} />
-                    <div className="mt-8">
-                        <h2 className="text-3xl font-bold mb-4">Location</h2>
-                        <img src={Image.LocationMap} alt="This is location" />
-                    </div>
+                </div>
+                <div className="mt-8 text-center text-white">
+                    <h2 className="text-6xl font-bold mb-4">Location</h2>
+                    <img src={Image.LocationMap} alt="This is location" />
                 </div>
             </div>
         </div>
